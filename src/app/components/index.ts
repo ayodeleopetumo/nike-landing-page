88bb1e714752564d91211f89a81e0b97cfd05798
Ayodele Opetumo <aopetumo@gmail.com>
2024-04-13 23:47:53 +0200

    move files to section directory and update barrel file

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,13 +1,12 @@
-// export file
-import { FooterComponent } from './footer/footer.component';
-import { CustomerReviewComponent } from './customer-review/customer-review.component';
-import { HeroComponent } from './hero/hero.component';
-import { PopularProductsComponent } from './popular-products/popular-products.component';
-import { ServicesComponent } from './services/services.component';
-import { SpecialOffersComponent } from './special-offers/special-offers.component';
-import { SubscribeComponent } from './subscribe/subscribe.component';
-import { SuperQualityComponent } from './super-quality/super-quality.component';
 import { NavComponent } from './nav/nav.component';
+import { CustomerReviewComponent } from './sections/customer-review/customer-review.component';
+import { FooterComponent } from './sections/footer/footer.component';
+import { HeroComponent } from './sections/hero/hero.component';
+import { PopularProductsComponent } from './sections/popular-products/popular-products.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { SpecialOffersComponent } from './sections/special-offers/special-offers.component';
+import { SubscribeComponent } from './sections/subscribe/subscribe.component';
+import { SuperQualityComponent } from './sections/super-quality/super-quality.component';
 
 export {
   CustomerReviewComponent,
